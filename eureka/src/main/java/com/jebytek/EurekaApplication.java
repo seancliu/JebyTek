49395cdeffb0a63b51b7bab520ce536366dc85a6
seancliu <seanchangliu@hotmail.com>
2020-07-02 19:44:18 -0400

    1. Fix the bug of registry failure 2. Optimize Log Display

--- eureka/src/main/java/com/jebytek/EurekaApplication.java
+++ eureka/src/main/java/com/jebytek/EurekaApplication.java
@@ -1,13 +1,23 @@
 package com.jebytek;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaApplication {
 
+	private static final Logger LOG = LoggerFactory.getLogger(EurekaApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaApplication.class, args);
+		SpringApplication app = new SpringApplication(EurekaApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		LOG.info("Launched Successfully!");
+		LOG.info("Eureka Address: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
 	}
 
 }
