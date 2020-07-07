9b929df633c6a3b93f5c1da79be7ef4604b89ef6
seancliu <seanchangliu@hotmail.com>
2020-07-07 18:53:44 -0400

    1. Add MyBatis Testcase: http://127.0.0.1:9001/system/test 2. Add Controller-Service-Mapper Architecture

--- system/src/main/java/com/jebytek/system/controller/TestController.java
+++ system/src/main/java/com/jebytek/system/controller/TestController.java
@@ -1,12 +1,20 @@
 package com.jebytek.system.controller;
 
+import com.jebytek.system.domain.Test;
+import com.jebytek.system.service.TestService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @RestController
 public class TestController {
+    @Resource
+    private TestService testService;
+
     @RequestMapping("/test")
-    public String test() {
-        return "success";
+    public List<Test> test() {
+        return testService.list();
     }
 }
---
+++ system/src/main/java/com/jebytek/system/mapper/TestMapper.java
@@ -0,0 +1,10 @@
+package com.jebytek.system.mapper;
+
+import com.jebytek.system.domain.Test;
+
+import java.util.List;
+
+public interface TestMapper {
+    public List<Test> list();
+}
+
