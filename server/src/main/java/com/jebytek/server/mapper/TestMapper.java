db4272589cdf494b70b03f165b61bdb3c602a8a5
seancliu <seanchangliu@hotmail.com>
2020-07-08 19:16:54 -0400

    1. Add MyBatis Generator

--- server/src/main/java/com/jebytek/server/mapper/TestMapper.java
+++ server/src/main/java/com/jebytek/server/mapper/TestMapper.java
@@ -1,10 +1,23 @@
 package com.jebytek.server.mapper;
 
 import com.jebytek.server.domain.Test;
+import com.jebytek.server.domain.TestExample;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface TestMapper {
-    public List<Test> list();
-}
+    long countByExample(TestExample example);
 
+    int deleteByExample(TestExample example);
+
+    int insert(Test record);
+
+    int insertSelective(Test record);
+
+    List<Test> selectByExample(TestExample example);
+
+    int updateByExampleSelective(@Param("record") Test record, @Param("example") TestExample example);
+
+    int updateByExample(@Param("record") Test record, @Param("example") TestExample example);
+}
--- server/src/main/java/com/jebytek/server/service/TestService.java
+++ server/src/main/java/com/jebytek/server/service/TestService.java
@@ -1,17 +1,15 @@
 package com.jebytek.server.service;
 
-import com.jebytek.server.domain.Test;
 import com.jebytek.server.mapper.TestMapper;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 @Service
 public class TestService {
     @Resource
     private TestMapper testMapper;
-    public List<Test> list() {
-        return testMapper.list();
-    }
+//    public List<Test> list() {
+//        return testMapper.list();
+//    }
 }
