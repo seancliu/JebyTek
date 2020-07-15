105dcf78b501224896e51e2b85022e10e3a71c0c
seancliu <seanchangliu@hotmail.com>
2020-07-15 17:51:58 -0400

    1. Add axios for communication between frontend and backend 2. Add CorsConfig to support Cross-Origin Resource Sharing

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -976,9 +976,17 @@
     export default {
         name: 'chapter',
         mounted: function() {
+            let _this = this;
+            _this.list();
             // this.$parent.activateSidebar("business-chapter-sidebar")
         },
         methods: {
+            list() {
+                let _this = this;
+                _this.$ajax.get('http://127.0.0.1:9002/business/admin/chapter/list').then((response)=>{
+                    console.log("Chapter Search Result: ", response);
+                })
+            }
         }
     }
 </script>
