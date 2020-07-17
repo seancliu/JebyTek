18793e70bd5296d3c5db603e18cd85e9528b7648
seancliu <seanchangliu@hotmail.com>
2020-07-17 16:56:59 -0400

    1. Add pagination module 'pagination.vue' 2. Add pagination module on chapter display page

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
     <p>
-      <button v-on:click="list()" class="btn btn-white btn-default btn-round">
+      <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
         <i class="ace-icon fa fa-refresh"></i>
         Refresh
       </button>
     </p>
+
+    <pagination ref="pagination" v-bind:list="list"></pagination>
+
     <table id="simple-table" class="table  table-bordered table-hover">
       <thead>
       <tr>
@@ -85,7 +88,9 @@
 </template>
 
 <script>
+    import Pagination from "../../components/pagination";
     export default {
+        components: {Pagination},
         name: 'chapter',
         data: function() {
             return {
@@ -95,18 +100,20 @@
 
         mounted: function() {
             let _this = this;
-            _this.list();
+            _this.$refs.pagination.size = 10;
+            _this.list(1);
             // this.$parent.activateSidebar("business-chapter-sidebar")
         },
         methods: {
-            list() {
+            list(page) {
                 let _this = this;
                 _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/list', {
-                    page: 1,
-                    size: 1
+                    page: page,
+                    size: _this.$refs.pagination.size,
                 }).then((response)=>{
                     console.log("Chapter Search Result: ", response);
                     _this.chapters = response.data.list;
+                    _this.$refs.pagination.render(page, response.data.total);
                 })
             }
         }
--- doc/db/all.sql
+++ doc/db/all.sql
@@ -14,8 +14,22 @@ create table `chapter` (
 
 
 
-insert into `chapter` (id, course_id, name) values ('00000000', '00000000', 'Test Chapter One');
-insert into `chapter` (id, course_id, name) values ('00000001', '00000000', 'Test Chapter Two');
+insert into `chapter` (id, course_id, name) values ('00000000', '00000000', 'Test Chapter 00');
+insert into `chapter` (id, course_id, name) values ('00000001', '00000000', 'Test Chapter 01');
+insert into `chapter` (id, course_id, name) values ('00000002', '00000000', 'Test Chapter 02');
+insert into `chapter` (id, course_id, name) values ('00000003', '00000000', 'Test Chapter 03');
+insert into `chapter` (id, course_id, name) values ('00000004', '00000000', 'Test Chapter 04');
+insert into `chapter` (id, course_id, name) values ('00000005', '00000000', 'Test Chapter 05');
+insert into `chapter` (id, course_id, name) values ('00000006', '00000000', 'Test Chapter 06');
+insert into `chapter` (id, course_id, name) values ('00000007', '00000000', 'Test Chapter 07');
+insert into `chapter` (id, course_id, name) values ('00000008', '00000000', 'Test Chapter 08');
+insert into `chapter` (id, course_id, name) values ('00000009', '00000000', 'Test Chapter 09');
+insert into `chapter` (id, course_id, name) values ('00000010', '00000000', 'Test Chapter 10');
+insert into `chapter` (id, course_id, name) values ('00000011', '00000000', 'Test Chapter 11');
+insert into `chapter` (id, course_id, name) values ('00000012', '00000000', 'Test Chapter 12');
+insert into `chapter` (id, course_id, name) values ('00000013', '00000000', 'Test Chapter 13');
+insert into `chapter` (id, course_id, name) values ('00000014', '00000000', 'Test Chapter 14');
+
 
 
 
