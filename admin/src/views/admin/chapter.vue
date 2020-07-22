e64edcf44756330bbf9fccd14c4ff357ee4035ab
seancliu <seanchangliu@hotmail.com>
2020-07-22 15:39:16 -0400

    1. Add confirmation alert when deleting a chapter using sweetalert2

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -173,13 +173,32 @@
 
             del(id) {
                 let _this = this;
-                _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/' + id).then((response)=>{
-                    console.log("Chapter Delete Result: ", response);
-                    let resp = response.data;
-                    if (resp.success) {
-                        _this.list(1);
+                Swal.fire({
+                    title: 'Are you sure?',
+                    text: "You won't be able to revert this!",
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#3085d6',
+                    cancelButtonColor: '#d33',
+                    confirmButtonText: 'Yes, delete it!'
+                }).then((result) => {
+                    if (result.value) {
+                        _this.$ajax.delete('http://127.0.0.1:9000/business/admin/chapter/delete/' + id).then((response)=>{
+                            console.log("Chapter Delete Result: ", response);
+                            let resp = response.data;
+                            if (resp.success) {
+                                _this.list(1);
+                                Swal.fire(
+                                    'Deleted!',
+                                    'Your file has been deleted.',
+                                    'success'
+                                )
+                            }
+                        })
+
                     }
                 })
+
             }
         }
     }
