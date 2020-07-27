cae5061d8f99663c387943f79111797351106661
seancliu <seanchangliu@hotmail.com>
2020-07-27 13:25:17 -0400

    1. Optimize ServerGenerator via extracting table information using MyBatis

--- generator/src/main/java/com/jebytek/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/jebytek/generator/server/ServerGenerator.java
@@ -3,7 +3,11 @@ package com.jebytek.generator.server;
 import com.jebytek.generator.util.DbUtil;
 import com.jebytek.generator.util.Field;
 import com.jebytek.generator.util.FreemarkerUtil;
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 
+import java.io.File;
 import java.util.*;
 
 public class ServerGenerator {
@@ -11,18 +15,41 @@ public class ServerGenerator {
     static String toDtoPath = "server\\src\\main\\java\\com\\jebytek\\server\\dto\\";
     static String toServicePath = "server\\src\\main\\java\\com\\jebytek\\server\\service\\";
     static String toControllerPath = MODULE + "\\src\\main\\java\\com\\jebytek\\" + MODULE + "\\controller\\admin\\";
+    static String generatorConfigPath = "server\\src\\main\\resources\\generator\\generatorConfig.xml";
+
     public static void main(String[] args) throws Exception {
-        String Domain = "Section";
-        String domain = "section";
         String module = MODULE;
-        List<Field> fieldList = DbUtil.getColumnByTableName(domain);
+
+        // 只生成配置文件中的第一个table节点
+        File file = new File(generatorConfigPath);
+        SAXReader reader=new SAXReader();
+        //读取xml文件到Document中
+        Document doc=reader.read(file);
+        //获取xml文件的根节点
+        Element rootElement=doc.getRootElement();
+        //读取context节点
+        Element contextElement = rootElement.element("context");
+        //定义一个Element用于遍历
+        Element tableElement;
+        //取第一个“table”的节点
+        tableElement=contextElement.elementIterator("table").next();
+        String Domain = tableElement.attributeValue("domainObjectName");
+        String tableName = tableElement.attributeValue("tableName");
+        String tableNameComment = DbUtil.getTableComment(tableName);
+        String domain = Domain.substring(0, 1).toLowerCase() + Domain.substring(1);
+        System.out.println("Table: "+tableElement.attributeValue("tableName"));
+        System.out.println("Domain: "+tableElement.attributeValue("domainObjectName"));
+
+        List<Field> fieldList = DbUtil.getColumnByTableName(tableName);
         Set<String> typeSet = getJavaTypes(fieldList);
         Map<String, Object> map = new HashMap<>();
         map.put("Domain", Domain);
         map.put("domain", domain);
         map.put("module", module);
+        map.put("tableNameComment", tableNameComment);
         map.put("fieldList", fieldList);
         map.put("typeSet", typeSet);
+
         // GENERATE SERVICE
         FreemarkerUtil.initConfig("dto.ftl");
         FreemarkerUtil.generator(toDtoPath + Domain + "Dto.java", map);
