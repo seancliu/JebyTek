a942f230cbd198c2dfaad55153b7c87d3998e754
seancliu <seanchangliu@hotmail.com>
2020-08-10 16:38:10 -0400

    1. Modify keys in sessionStorage as constants

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -141,7 +141,7 @@
         mounted: function() {
             let _this = this;
             _this.$refs.pagination.size = 10;
-            let course = SessionStorage.get("course" || {});
+            let course = SessionStorage.get(SESSION_KEY_COURSE || {});
             if (Tool.isEmpty(course)) {
                 _this.$router.push("/dashboard");
             }
@@ -234,7 +234,7 @@
              */
             toSection(chapter) {
                 let _this = this;
-                SessionStorage.set("chapter", chapter);
+                SessionStorage.set(SESSION_KEY_CHAPTER, chapter);
                 _this.$router.push("/business/section");
             },
         }
