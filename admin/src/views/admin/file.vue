8abd660056ca93500b4640cf0f59143248faf2f8
seancliu <seanchangliu@hotmail.com>
2020-08-18 13:08:26 -0400

    1. Change file management page to read-only

--- admin/src/views/admin/file.vue
+++ admin/src/views/admin/file.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
     <p>
-      <button v-on:click="add()" class="btn btn-white btn-default btn-round">
-        <i class="ace-icon fa fa-edit"></i>
-        Add
-      </button>
-      &nbsp;
       <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
         <i class="ace-icon fa fa-refresh"></i>
         Refresh
@@ -29,8 +24,6 @@
 
             <th>Use</th>
 
-
-            <th>Operation</th>
       </tr>
       </thead>
 
@@ -44,106 +37,11 @@
         <td>{{FILE_USE | optionKV(file.use)}}</td>
 
 
-        <td>
-          <div class="hidden-sm hidden-xs btn-group">
-            <button v-on:click="edit(file)" class="btn btn-xs btn-info">
-              <i class="ace-icon fa fa-pencil bigger-120"></i>
-            </button>
-
-            <button v-on:click="del(file.id)" class="btn btn-xs btn-danger">
-              <i class="ace-icon fa fa-trash-o bigger-120"></i>
-            </button>
-          </div>
-
-          <div class="hidden-md hidden-lg">
-            <div class="inline pos-rel">
-              <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
-                <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
-              </button>
-
-              <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
-                <li>
-                  <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
-                    <span class="blue">
-                        <i class="ace-icon fa fa-search-plus bigger-120"></i>
-                    </span>
-                  </a>
-                </li>
-
-                <li>
-                  <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
-                    <span class="green">
-                        <i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
-                    </span>
-                  </a>
-                </li>
-
-                <li>
-                  <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
-                    <span class="red">
-                        <i class="ace-icon fa fa-trash-o bigger-120"></i>
-                    </span>
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </td>
       </tr>
 
       </tbody>
     </table>
 
-    <div id="form-modal" class="modal fade" tabindex="-1" role="dialog">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title">Add a file</h4>
-          </div>
-          <div class="modal-body">
-            <form class="form-horizontal">
-                <div class="form-group">
-                  <label class="col-sm-2 control-label">Relative Path</label>
-                  <div class="col-sm-10">
-                    <input v-model="file.path" class="form-control">
-                  </div>
-                </div>
-                <div class="form-group">
-                  <label class="col-sm-2 control-label">File Name</label>
-                  <div class="col-sm-10">
-                    <input v-model="file.name" class="form-control">
-                  </div>
-                </div>
-                <div class="form-group">
-                  <label class="col-sm-2 control-label">File Extension</label>
-                  <div class="col-sm-10">
-                    <input v-model="file.suffix" class="form-control">
-                  </div>
-                </div>
-                <div class="form-group">
-                  <label class="col-sm-2 control-label">Size</label>
-                  <div class="col-sm-10">
-                    <input v-model="file.size" class="form-control">
-                  </div>
-                </div>
-                <div class="form-group">
-                    <label class="col-sm-2 control-label">Use</label>
-                    <div class="col-sm-10">
-                      <select v-model="file.use" class="form-control">
-                        <option v-for="o in FILE_USE" v-bind:value="o.key">{{o.value}}</option>
-                      </select>
-                    </div>
-                </div>
-            </form>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
-            <button v-on:click="save()" type="button" class="btn btn-primary">Save</button>
-          </div>
-        </div><!-- /.modal-content -->
-      </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
   </div>
 
 </template>
@@ -168,17 +66,6 @@
             // this.$parent.activateSidebar("file-file-sidebar")
         },
         methods: {
-            add() {
-                let _this = this;
-                _this.file = {};
-                $("#form-modal").modal("show");
-            },
-
-            edit(file) {
-                let _this = this;
-                _this.file = $.extend({}, file);
-                $("#form-modal").modal("show");
-            },
 
             /*
             * get all files
@@ -197,35 +84,6 @@
                 })
             },
 
-            /*
-            * save change to a file
-            * */
-            save() {
-                let _this = this;
-
-                //save validation
-                if (1 != 1
-                    || !Validator.require(_this.file.path, "Relative Path")
-                    || !Validator.length(_this.file.path, "Relative Path", 1, 100)
-                    || !Validator.length(_this.file.name, "File Name", 1, 100)
-                    || !Validator.length(_this.file.suffix, "File Extension", 1, 10)
-                ) {
-                  return;
-                }
-
-                Loading.show();
-                _this.$ajax.post(process.env.VUE_APP_SERVER + '/file/admin/file/save', _this.file).then((response)=>{
-                    Loading.hide();
-                    let resp = response.data;
-                    if (resp.success) {
-                        $("#form-modal").modal("hide");
-                        _this.list(1);
-                        Toast.success("Saved successfully!");
-                    } else {
-                        Toast.warning(resp.message)
-                    }
-                })
-            }       ,
 
             /*
             * delete a file
--- file/src/main/java/com/jebytek/file/controller/admin/FileController.java
+++ file/src/main/java/com/jebytek/file/controller/admin/FileController.java
@@ -1,11 +1,12 @@
 package com.jebytek.file.controller.admin;
 
-import com.jebytek.server.dto.FileDto;
 import com.jebytek.server.dto.PageDto;
 import com.jebytek.server.dto.ResponseDto;
 import com.jebytek.server.service.FileService;
-import com.jebytek.server.util.ValidatorUtil;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
@@ -23,36 +24,6 @@ public class FileController {
         ResponseDto responseDto = new ResponseDto();
         fileService.list(pageDto);
         responseDto.setContent(pageDto);
-        return responseDto;
-    }
-
-    /*
-    * save a record
-    * when id is empty, insert;
-    * when id is not empty, update
-    * */
-    @PostMapping("/save")
-    public ResponseDto save(@RequestBody FileDto fileDto) {
-
-        // saving validation
-        ValidatorUtil.require(fileDto.getPath(), "Relative Path");
-        ValidatorUtil.length(fileDto.getPath(), "Relative Path", 1, 100);
-        ValidatorUtil.length(fileDto.getName(), "File Name", 1, 100);
-        ValidatorUtil.length(fileDto.getSuffix(), "File Extension", 1, 10);
-
-        ResponseDto responseDto = new ResponseDto();
-        fileService.save(fileDto);
-        responseDto.setContent(fileDto);
-        return responseDto;
-    }
-
-    /*
-    * delete
-    * */
-    @DeleteMapping("/delete/{id}")
-    public ResponseDto delete(@PathVariable String id) {
-        ResponseDto responseDto = new ResponseDto();
-        fileService.delete(id);
         return responseDto;
     }
 }
